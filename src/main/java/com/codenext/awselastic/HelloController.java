1cda8a195a2906c57020b897f63d938f49e2f6a3
smurugesan84 <sivabalan.murugesan@aimia.com>
2020-05-25 15:52:53 +0100

    seven commit

--- src/main/java/com/codenext/awselastic/HelloController.java
+++ src/main/java/com/codenext/awselastic/HelloController.java
@@ -1,5 +1,7 @@
 package com.codenext.awselastic;
 
+import static java.time.ZonedDateTime.now;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,10 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String getHelloWorld(){
-        return "The demo of hello world";
+        return "The demo of hello world " + now();
     }
     @GetMapping("/{id}")
     public String getHelloWorld(@PathVariable("id") String id){
-        return "The demo of hello world " + id;
+        return "The demo of hello world  " + id;
     }
 }
