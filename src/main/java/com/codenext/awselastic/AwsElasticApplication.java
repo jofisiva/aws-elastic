f1ff452ed82f64e276a9f566f3fece2bacd3593b
smurugesan84 <sivabalan.murugesan@aimia.com>
2020-05-25 14:23:48 +0100

    Initial commit

---
+++ src/main/java/com/codenext/awselastic/AwsElasticApplication.java
@@ -0,0 +1,13 @@
+package com.codenext.awselastic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsElasticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsElasticApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/codenext/awselastic/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.codenext.awselastic;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AwsElasticApplication.class);
+	}
+
+}
