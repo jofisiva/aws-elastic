752b47331b72ce575418e73c5428175c7a32cf56
smurugesan84 <sivabalan.murugesan@aimia.com>
2020-05-25 15:25:59 +0100

    second commit

--- src/main/java/com/codenext/awselastic/HelloController.java
+++ src/main/java/com/codenext/awselastic/HelloController.java
@@ -2,6 +2,7 @@ package com.codenext.awselastic;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +11,9 @@ public class HelloController {
     @GetMapping("/")
     public String getHelloWorld(){
         return "The demo of hello world";
+    }
+    @GetMapping("/{id}")
+    public String getHelloWorld(@RequestParam("id") String id){
+        return "The demo of hello world " + id;
     }
 }
